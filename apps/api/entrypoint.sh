c24246d2bed8cb0049fc4fc759ba4275b4e91a17
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2026-05-30 20:49:14 +0200

    fix prod error

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migration (prisma db push)..."
-npx prisma db push --schema=./prisma/schema.prisma --accept-data-loss
+npx prisma db push --schema=./prisma/schema.prisma --config=./prisma.config.ts --accept-data-loss
 
 echo "Starting server..."
 exec node dist/server.js
